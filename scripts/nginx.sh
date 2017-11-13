edd0704fd3388c22e9c4f3dc3c5d6d5556666b86
Jaymz Rhime <wxactly@gmail.com>
2017-11-13 10:58:34 -0600

    Fixing nginx installed check, and USERHOME replacement

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-installed=`which nginx`
-if [ "x$installed" != "x" ] ; then
-	echo $'\n'
+if brew list nginx; then
+	echo ""
 	echo "Configuring nginx"
-	echo $'\n'
+	echo ""
 
 	sudo launchctl unload /System/Library/LaunchDaemons/org.apache.httpd.plist 2>/dev/null
 
@@ -12,16 +11,16 @@ if [ "x$installed" != "x" ] ; then
 
 	export USERHOME=$(dscl . -read /Users/`whoami` NFSHomeDirectory | awk -F"\: " '{print $2}')
 
-	$(brew --prefix gettext)/bin/envsubst < ~/ts_environment/config/nginx.conf > $(brew --prefix)/etc/nginx/nginx.conf
-	$(brew --prefix gettext)/bin/envsubst < ~/ts_environment/config/ts-dev.conf > $(brew --prefix)/etc/nginx/ts-dev.conf
+	cat config/nginx.conf > $(brew --prefix)/etc/nginx/nginx.conf
+  sed -i -e "s#\${USERHOME}#$USERHOME#g" $(brew --prefix)/etc/nginx/nginx.conf
+
+	cat config/ts-dev.conf > $(brew --prefix)/etc/nginx/ts-dev.conf
+  sed -i -e "s#\${USERHOME}#$USERHOME#g" $(brew --prefix)/etc/nginx/ts-dev.conf
 
 	mkdir -p $(brew --prefix)/etc/nginx/ssl
 
 	openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=State/L=Town/O=Office/CN=dev" -keyout $(brew --prefix)/etc/nginx/ssl/dev.key -out $(brew --prefix)/etc/nginx/ssl/dev.crt
 
 	brew services start nginx
-else
-	echo $'\n'
-	echo "nginx is not installed - skipping"
-	echo $'\n'
 fi
+
