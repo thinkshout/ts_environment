8dba98c15978fe8b334ca02cdc596a3c529551da
Tauno Hogue <tauno@thinkshout.com>
2018-04-24 12:38:35 -0700

    Fix bash syntax and drop redis since it prompts for answers and likely isn't needed

--- scripts/php.sh
+++ scripts/php.sh
@@ -6,19 +6,18 @@ if brew list php; then
   echo $'\n'
 
   echo $'\n'
-  echo "Installing PECL extensions (xdebug and redis) for each version of PHP"
+  echo "Installing PECL extensions (xdebug) for each version of PHP"
   echo $'\n'
   brew unlink php@7.2
   for VER in 5.6 7.0 7.1 7.2
   do
     brew link --force php@$VER
     php -v
-    if [ $VER == '5.6' ]
-      pecl install xdebug-2.5.5
+    if [ $VER == '5.6' ]; then
+      pecl install -f xdebug-2.5.5
     else
-      pecl install xdebug
+      pecl install -f xdebug
     fi
-    pecl install redis
     brew unlink php@$VER
   done
 
