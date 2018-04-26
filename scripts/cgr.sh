fec44f351eb66fb7fca2e99d437888792705d83e
Gabe Carleton-Barnes <gabe@thinkshout.com>
2018-04-26 17:46:07 -0400

    Configure codesniffer.

--- scripts/cgr.sh
+++ scripts/cgr.sh
@@ -11,6 +11,8 @@ cgr consolidation/Robo
 cgr drush/drush "^8.0"
 cgr drupal/console
 
+phpcs --config-set installed_paths ~/.composer/global/drupal/coder/vendor/drupal/coder/coder_sniffer
+
 echo ""
 echo "Please ensure that your shell is configured to always include './vendor/bin:~/.composer/vendor/bin' in your PATH"
 echo ""
