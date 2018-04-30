a3e54dfd827bcf9985c44dda64b865de13b77a12
Jaymz Rhime <wxactly@gmail.com>
2018-04-30 10:26:54 -0500

    Revert "Add symlinks for drupal and wordpress standards"
    This reverts commit 2a3dc0c31a0211da09cc6d39010e993d4627404f.

--- scripts/cgr.sh
+++ scripts/cgr.sh
@@ -3,7 +3,6 @@
 export PATH=./vendor/bin:~/.composer/vendor/bin:$PATH
 
 composer global require consolidation/cgr
-
 cgr squizlabs/php_codesniffer "^2.9"
 cgr drupal/coder
 cgr wp-coding-standards/wpcs
@@ -12,14 +11,7 @@ cgr consolidation/Robo
 cgr drush/drush "^8.0"
 cgr drupal/console
 
-ln -s ~/.composer/global/drupal/coder/vendor/drupal/coder/coder_sniffer/Drupal $(brew --prefix)/etc/php-code-sniffer/Standards/
-ln -s ~/.composer/global/drupal/coder/vendor/drupal/coder/coder_sniffer/DrupalPractice $(brew --prefix)/etc/php-code-sniffer/Standards/
-
-ln -s ~/.composer/global/wp-coding-standards/wpcs/vendor/wp-coding-standards/wpcs/WordPress $(brew --prefix)/etc/php-code-sniffer/Standards/
-ln -s ~/.composer/global/wp-coding-standards/wpcs/vendor/wp-coding-standards/wpcs/WordPress-Core $(brew --prefix)/etc/php-code-sniffer/Standards/
-ln -s ~/.composer/global/wp-coding-standards/wpcs/vendor/wp-coding-standards/wpcs/WordPress-Docs $(brew --prefix)/etc/php-code-sniffer/Standards/
-ln -s ~/.composer/global/wp-coding-standards/wpcs/vendor/wp-coding-standards/wpcs/WordPress-Extra $(brew --prefix)/etc/php-code-sniffer/Standards/
-ln -s ~/.composer/global/wp-coding-standards/wpcs/vendor/wp-coding-standards/wpcs/WordPress-VIP $(brew --prefix)/etc/php-code-sniffer/Standards/
+phpcs --config-set installed_paths ~/.composer/global/drupal/coder/vendor/drupal/coder/coder_sniffer
 
 echo ""
 echo "Please ensure that your shell is configured to always include './vendor/bin:~/.composer/vendor/bin' in your PATH"
