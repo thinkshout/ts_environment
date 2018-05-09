1c2f49093b9234334a4a82d4a60e973e3ba36abb
Jaymz Rhime <wxactly@gmail.com>
2018-05-09 08:20:46 -0500

    Update drushrc file to automatically detect and configure uri option

--- config/drushrc.php
+++ config/drushrc.php
@@ -1,6 +1,21 @@
 <?php
 
-if (file_exists(getcwd() . '/web')) {
-  $options['root'] = getcwd() . '/web';
+// Determine if we're in ~/Sites.
+if (preg_match('#' . drush_server_home() . '/Sites/([^/]+)/?(web)?#', getcwd(), $path_matches)) {
+  // Find the site name and web directory, if it exists.
+  list($match, $site, $web_in_cwd) = $path_matches;
+  $web_in_subdir = file_exists(getcwd() . '/web');
+  
+  // Set uri for localhost.
+  if ($web_in_cwd || $web_in_subdir) {
+    $options['uri'] = 'https://web.' . $site . '.localhost';
+  }
+  else {
+    $options['uri'] = 'https://' . $site . '.localhost';
+  }
+  
+  // Set web root.
+  if ($web_in_subdir) {
+    $options['root'] = getcwd() . '/web';
+  }
 }
-
