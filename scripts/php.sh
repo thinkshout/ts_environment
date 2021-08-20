b157640745c69ac9d44b80a3c3a320d25c3a091b
Jaymz Rhime <wxactly@gmail.com>
2021-08-20 14:33:08 -0500

    Adding php 8.0 to config steps as well

--- scripts/php.sh
+++ scripts/php.sh
@@ -11,7 +11,7 @@ if brew list php; then
 
   brew unlink php
 
-  for VER in 7.2 7.3 7.4
+  for VER in 7.2 7.3 7.4 8.0
   do
     brew link --force php@$VER
     php -v
@@ -23,7 +23,7 @@ if brew list php; then
   echo "Installing TS config for each version of PHP"
   echo $'\n'
 
-  for VER in 7.2 7.3 7.4
+  for VER in 7.2 7.3 7.4 8.0
   do
     $(brew --prefix gettext)/bin/envsubst < config/php-ts.ini > $(brew --prefix)/etc/php/$VER/conf.d/php-ts.ini
   done
