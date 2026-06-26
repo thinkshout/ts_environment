4bf97ce942add29a0e1ff6d16b392f8f524d7779
Jaymz Rhime <wxactly@gmail.com>
2026-06-26 12:08:00 -0500

    Remove EOL php versions and add new versions

--- scripts/php.sh
+++ scripts/php.sh
@@ -11,7 +11,7 @@ if brew list php; then
 
   brew unlink php
 
-  for VER in 7.4 8.1 8.2 8.3
+  for VER in 8.1 8.2 8.3 8.4 8.5
   do
     brew link --force php@$VER
     php -v
@@ -23,18 +23,18 @@ if brew list php; then
   echo "Installing TS config for each version of PHP"
   echo $'\n'
 
-  for VER in 7.4 8.0 8.1 8.2 8.3
+  for VER in 8.1 8.2 8.3 8.4 8.5
   do
     $(brew --prefix gettext)/bin/envsubst < config/php-ts.ini > $(brew --prefix)/etc/php/$VER/conf.d/php-ts.ini
   done
 
   # Install cgr scripts under oldest php version for backwards compat
   brew unlink php
-  brew link --force php@7.4
+  brew link --force php@8.1
 
   source scripts/cgr.sh
 
-  brew unlink php@7.4
+  brew unlink php@8.1
 
   echo $'\n'
   echo "Starting PHP."
